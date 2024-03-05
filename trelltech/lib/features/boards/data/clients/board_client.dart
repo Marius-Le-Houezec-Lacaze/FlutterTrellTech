ae274b69c6582bdc6f06108fb0bde5d8a25cdba0
Marius Le Houezec Lacaze <mlehouezeclacaze@lucca.fr>
2024-03-05 15:23:40 +0100

    feat(boards): api call to list a organization board

--- trelltech/lib/features/boards/data/clients/board_client.dart
+++ trelltech/lib/features/boards/data/clients/board_client.dart
@@ -1,18 +1,29 @@
 import 'package:dio/dio.dart';
+import 'package:result_dart/result_dart.dart';
 import 'package:trelltech/config/constant.dart';
+import 'package:trelltech/features/boards/data/clients/dtos/board_dto.dart';
 
 class BoardsClient {
 
   final Dio _dio;
   BoardsClient(this._dio);
 
-  final String url = '${baseUrl}';
+  final String url = baseUrl;
 
-  getBoardByOrganizationId(
+  Future<Result<List<BoardDto>,DioException>> getBoardByOrganizationId(
       String organizationId
       ) async {
-    final result = await _dio.get("organization/$url/$organizationId/boards");
+    try{
+      final result = await _dio.get<List<Map<String, dynamic>>>("${url}organization/$organizationId/boards");
+
+      if(result.data == null){
+        return Failure(DioException(requestOptions: result.requestOptions));
+      }
+
+      final boards = result.data!.map<BoardDto>((json) => BoardDto.fromJson(json)).toList();
+      return Success(boards);
+    } on DioException catch(e){
+      return Failure(e);
+    }
   }
-
-
 }
--- trelltech/lib/features/boards/data/clients/dtos/board_dto.dart
+++ trelltech/lib/features/boards/data/clients/dtos/board_dto.dart
@@ -1,14 +1,14 @@
 
 
-class BoardsDto {
+class BoardDto {
 
   final String ? id;
   final String ? name;
   final String ? description;
 
-  BoardsDto(this.name, this.description, {required this.id});
+  BoardDto(this.name, this.description, {required this.id});
 
-  static fromJson(Map<String, dynamic> json) => BoardsDto(
+  static fromJson(Map<String, dynamic> json) => BoardDto(
     json['name'] ?? "",
     json['description'] ?? "",
     id: json['id'] ?? "",
