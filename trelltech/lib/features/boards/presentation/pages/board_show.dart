d701edc448c8a16e7f37e79292231f9e96d508a7
Marius Le Houezec Lacaze <mlehouezeclacaze@lucca.fr>
2024-03-12 15:06:50 +0100

    wip

---
+++ trelltech/lib/features/boards/presentation/pages/board_show.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/material.dart';
+
+class BoardShow extends StatefulWidget{
+  const BoardShow({super.key});
+
+  @override
+  State<BoardShow> createState() => _BoardShowState();
+}
+
+class _BoardShowState extends State<BoardShow> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
