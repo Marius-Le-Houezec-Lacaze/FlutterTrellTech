2dace7ee3883282e195208f54b1a18f701626fbb
JCR <jeanclementeradigois@gmail.com>
2024-03-12 10:56:55 +0100

    test: create tests folder and files

---
+++ trelltech/flutter_test/TU1.dart
@@ -0,0 +1,10 @@
+import 'package:test/test.dart';
+import 'package:trelltech/features/auth/presentation/pages/auth.dart';
+
+void main() {
+  test('Counter value should be incremented', () {
+    final auth = Auth();
+
+
+  });
+}
