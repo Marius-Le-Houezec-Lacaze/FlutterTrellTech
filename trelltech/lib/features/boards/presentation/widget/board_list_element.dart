9dedeb2682e9e0b55ac61faf8226fbd1a7581596
zouzou <zoevoile@gmail.com>
2024-03-19 17:15:16 +0100

    style: adding style to workspace & board

--- trelltech/lib/features/boards/presentation/widget/board_list_element.dart
+++ trelltech/lib/features/boards/presentation/widget/board_list_element.dart
@@ -4,36 +4,43 @@ import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:trelltech/features/boards/domain/arguments/board_show_argument.dart';
 import 'package:trelltech/features/boards/domain/entities/board_entity.dart';
 
-class BoardListElement extends StatelessWidget{
+class BoardListElement extends StatelessWidget {
   BoardEntity boardEntity;
 
   BoardListElement(this.boardEntity, {super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    return Slidable(
-        direction: Axis.horizontal,
-        endActionPane:  const ActionPane(
-          motion: ScrollMotion(),
-          children: [
-            // ..._buildActions(),
-          ],
-        ),
-        child: AnimatedContainer(
-            height: 60,
-            duration: const Duration(seconds:2),
-            child: Material(
-              color:Theme.of(context).colorScheme.primary,
-              child: InkWell(
-                onTap: (){
-                  Navigator.pushNamed(context, "/boardshow", arguments: BoardShowArgument(boardEntity.id!));
-                },
-                child: Center(child: Text('Entry ${boardEntity.name}')),
-              ),
-            )
-        )
-    );
+    return Padding(
+        padding: EdgeInsets.symmetric(vertical: 4),
+        child: Slidable(
+            direction: Axis.horizontal,
+            endActionPane: const ActionPane(
+              motion: ScrollMotion(),
+              children: [
+                // ..._buildActions(),
+              ],
+            ),
+            child: AnimatedContainer(
+                height: 60,
+                duration: const Duration(seconds: 2),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.primary,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.pushNamed(context, "/boardshow",
+                          arguments: BoardShowArgument(boardEntity.id!));
+                    },
+                    child: Center(
+                        child: Text(
+                      'Entry ${boardEntity.name}',
+                      style: TextStyle(color: Colors.white),
+                    )),
+                  ),
+                ))));
   }
-
 }
--- trelltech/lib/features/organization/presentation/widgets/organization_list_element.dart
+++ trelltech/lib/features/organization/presentation/widgets/organization_list_element.dart
@@ -22,7 +22,9 @@ class OrganizationListElement extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Slidable(
+    return Container(
+    margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+    child: Slidable(
         direction: Axis.horizontal,
         endActionPane:  ActionPane(
 
@@ -35,16 +37,17 @@ class OrganizationListElement extends StatelessWidget {
             height: 60,
             duration: const Duration(seconds:2),
             child: Material(
-              color:Theme.of(context).colorScheme.primary,
-              child: InkWell(
-                onTap: (){
-                  Navigator.pushNamed(context, "/boards", arguments: OrganizationArguments(organizationEntity.id!));
-                },
-                child: Center(child: Text('Entry ${organizationEntity.displayName}')),
-              ),
-            )
-        )
-    );
+              borderRadius: BorderRadius.circular(10),
+                      color:Theme.of(context).colorScheme.primary,
+                      child: InkWell(
+                        onTap: (){
+                          Navigator.pushNamed(context, "/boards", arguments: OrganizationArguments(organizationEntity.id!));
+                        },
+                        child: Center(
+                            child: Text('Entry ${organizationEntity.displayName}', style: TextStyle(color: Colors.white),)),
+                      ),
+                    )
+    )));
   }
 
   List<SlidableAction> _buildActions() {
@@ -57,6 +60,7 @@ class OrganizationListElement extends StatelessWidget {
           }
         },
         backgroundColor: const Color(0xFFFE4A49),
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(10), bottomLeft: Radius.circular(10)),
         foregroundColor: Colors.white,
         icon: Icons.delete,
         label: 'Delete',
@@ -69,6 +73,7 @@ class OrganizationListElement extends StatelessWidget {
           });
         },
         backgroundColor: const Color(0xFF21B7CA),
+        borderRadius: BorderRadius.only(topRight: Radius.circular(10), bottomRight: Radius.circular(10)),
         foregroundColor: Colors.white,
         icon: Icons.edit,
 
